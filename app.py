19f0c261ad6558843ac14c1e910f4aaef79de652
dvdwllc <wallacdc@Davids-MBP.hsd1.md.comcast.net>
2015-12-06 21:15:20 -0500

    changed index routing

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from bokeh.resources import INLINE
 
 app = Flask(__name__)
 
-@app.route('/index', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
 	return render_template('index.html')
 
